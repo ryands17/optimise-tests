cacbeb66b699a206583327714967db60ebd16e30
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2021-07-26 23:11:52 +0530

    add subtraction

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,5 +1,11 @@
-import { add } from '../src/add';
+import { add, subtract } from '../src/add';
 
 test('Sucessfully adds two numbers', () => {
   expect(add(2, 3)).toEqual(5);
+});
+
+test('Sucessfully subtracts two numbers', () => {
+  expect(subtract(10, 4)).toEqual(6);
+
+  expect(subtract(4, 8)).toEqual(-4);
 });
